fde4f89d6633ead94524bafd424c6b69eeb40ba0
Manik Fernando <mgalkissa@gmail.com>
2025-03-02 17:55:37 -0500

    add command line message passing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use dotenv::dotenv;
 use reqwest::blocking::Client;
-use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION, CONTENT_TYPE};
+use reqwest::header::{AUTHORIZATION, CONTENT_TYPE};
 use serde_json::json;
-use std::fs::File;
 use serde_json::Value;
+use clap::Parser;
 
 struct APICall {
     url: String,
@@ -35,15 +35,27 @@ fn get_chat_call(api_token:String,message:String,system_prompt:Option<String>) -
     }
 }
 
+#[derive(Parser)]
+#[command(name = "llm-client")]
+struct Args{
+    message: String,
+    system_prompt: Option<String>,
+}
+
 
 fn main() {
+    let args = Args::parse();
+    let message = args.message;
+
+    println!("Message: {}",message);
+
     dotenv().ok();
     let api_token = std::env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY must be set");
     // let api_token = api_token_result.expect("OPENAI_API_KEY must be set");
 
     let client = Client::new();
     
-    let test_call: APICall = get_chat_call(api_token, "Whats the square root of -1".to_string(), None);
+    let test_call: APICall = get_chat_call(api_token,message, None);
 
     
     let resp = client
@@ -60,5 +72,10 @@ fn main() {
     
     let body: Value = serde_json::from_str(&body).expect("Failed to parse JSON");
 
-    println!("Response: {}",body.get("choices").unwrap().to_string());
+    if let Some(content) = body["choices"][0]["message"]["content"].as_str(){
+        println!(">");
+        println!("{}",content);
+    }else{
+        println!("No content found");
+    }
 }
