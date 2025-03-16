ad9d78cfea3bf9a7e51ede3610759cea1a6528e9
Manik Fernando <mgalkissa@gmail.com>
2025-03-16 12:13:26 -0400

    Removed unused function

--- src/api.rs
+++ src/api.rs
@@ -70,24 +70,3 @@ pub struct APICall {
     pub authorization: String,
     pub json_body: serde_json::Value,
 }
-
-pub fn get_chat_call(api_token: String, message: String, system_prompt: Option<String>) -> APICall {
-    APICall {
-        url: String::from("https://api.openai.com/v1/chat/completions"),
-        content_type: String::from("application/json"),
-        authorization: format!("Bearer {}", api_token),
-        json_body: json!({
-            "model": "gpt-4o",
-            "messages": [
-                {
-                    "role": "system",
-                    "content": system_prompt.unwrap_or(String::from("You are a helpful assistant."))
-                },
-                {
-                    "role": "user",
-                    "content": message
-                }
-            ]
-        }),
-    }
-}
